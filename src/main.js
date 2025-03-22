9ab8dad6c3a78372b89b059dff29139bdec9c8e6
Khurshidbek-Ethan <ethankhurshidbek@gmail.com>
2025-03-22 15:14:35 +0900

    I did coding Optimizatsiya

--- src/main.js
+++ src/main.js
@@ -1,58 +1,73 @@
 // import { useReducer, useState } from 'react'
-import { createStore } from 'redux'
-const initialState = { count: 0, firstName: 'Ethan', lastName: 'Arapov' }
-
-const reducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'INC':
-			return {
-				...state,
-				count: state.count + 1,
-			}
-		case 'DECR':
-			return {
-				...state,
-				count: state.count - 1,
-			}
-		case 'RND':
-			return {
-				...state,
-				count: action.payload,
-			}
-	}
-}
+import { createStore, bindActionCreators } from 'redux'
+// import { inc, decr, rnd } from '../actions'
+import * as actions from '../actions'
+import reducer from '../reducer'
 
 // let state = reducer(initialState, { type: 'INC' })
 
 // const [state, dispatch] = useReducer(reducer,{type:"INC"})
 
 const store = createStore(reducer)
+const { getState, subscribe, dispatch } = store
 
 const updateUI = () => {
-	document.querySelector('#counter').textContent = store.getState().count
+	document.querySelector('#counter').textContent = getState().count
 }
 
 //subscribe obuna bb olamiz storega bizi statetimi xar  update bolganda yani setState bolganda logda korishimiz mumkin
-store.subscribe(updateUI)
+subscribe(updateUI)
 // console.log(store)
 
 //  dispatch da statetimizni qiymatini ozgartiramiz
 // store.dispatch({ type: 'INC' })
 // store.dispatch({ type: 'INC' })
 
-const inc = () => ({ type: 'INC' }),
-	decr = () => ({ type: 'DECR' }),
-	rnd = value => ({ type: 'RND', payload: value })
+// const bindActionCreator =
+// 	(creator, dispatch) =>
+// 	(...args) => {
+// 		dispatch(creator(...args))
+// 	}
 
-document.querySelector('#inc').addEventListener('click', () => {
-	store.dispatch(inc())
-})
+const { inc, decr, rnd } = bindActionCreators(actions, dispatch)
 
-document.querySelector('#decr').addEventListener('click', () => {
-	store.dispatch(decr())
-})
+// const { incDispatch, decrDispatch, rndDispatch } = bindActionCreators(
+// 	{
+// 		incDispatch: inc,
+// 		decrDispatch: decr,
+// 		rndDispatch: rnd,
+// 	},
+// 	dispatch
+// )
+// const decrDispatch = bindActionCreators(decr, dispatch)
+// const rndDispatch = bindActionCreators(rnd, dispatch)
+
+// const incDispatch = bindActionCreators(inc, dispatch)
+// const decrDispatch = bindActionCreators(decr, dispatch)
+// const rndDispatch = bindActionCreators(rnd, dispatch)
+
+// const incDispatch = bindActionCreator(inc, dispatch)
+// const decrDispatch = bindActionCreator(decr, dispatch)
+// const rndDispatch = bindActionCreator(rnd, dispatch)
+
+// const incDispatch = () => dispatch(inc())
+// const decrDispatch = () => dispatch(decr())
+// const rndDispatch = value => dispatch(rnd(value))
+
+document.querySelector('#inc').addEventListener('click', inc)
+
+document.querySelector('#decr').addEventListener('click', decr)
 
 document.querySelector('#rnd').addEventListener('click', () => {
 	const randomValue = Math.floor(Math.random() * 100)
-	store.dispatch(rnd(randomValue))
+	rnd(randomValue)
 })
+
+// document.querySelector('#inc').addEventListener('click', incDispatch)
+
+// document.querySelector('#decr').addEventListener('click', decrDispatch)
+
+// document.querySelector('#rnd').addEventListener('click', () => {
+// 	const randomValue = Math.floor(Math.random() * 100)
+// 	rndDispatch(randomValue)
+// })
